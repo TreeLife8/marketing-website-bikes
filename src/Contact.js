df46b1ff193d6a5d251c68e43c5dfa27f7aa9f42
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-10-12 13:33:00 +1100

    updated the picture on the contact page to come in from the side

--- src/Contact.js
+++ src/Contact.js
@@ -9,30 +9,30 @@ export default function Contact() {
   });
   return (
     <div className="Contact container-fluid">
-      <h1>CONTACT</h1>
-      <p>
-        Need to tell us something?
-        <br />
-        Send us an email through this form
-      </p>
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-sm-7">
+          <h1>CONTACT</h1>
+          <p>
+            Need to tell us something?
+            <br />
+            Send us an email through this form
+          </p>
           <form>
             <input
-              name="yourName"
+              name="name"
               className="form form-control"
               type="text"
               placeholder="name..."
             ></input>
             <input
-              name="yourNumber"
+              name="number"
               className="form form-control"
               type="number"
               placeholder="phone number..."
             ></input>
             <textarea
               className="form form-control"
-              name="yourMessage"
+              name="message"
               placeholder="message..."
               rows="5"
               cols="30"
@@ -44,12 +44,12 @@ export default function Contact() {
             ></input>
           </form>
         </div>
-        <div className="col-sm-6">
+        <div className="col-sm-5 bike-image">
           <img
-            data-aos="fade-left"
+            data-aos="fade-up"
             src={Bike}
             alt="bike handles"
-            className="img-fluid"
+            className="img-fluid rotate"
           />
         </div>
       </div>
